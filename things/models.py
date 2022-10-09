d72c22372bdcce253c6db8f2b9e9365825690d3d
Abbas BinVakas <abbas.bin_vakas@kcl.ac.uk>
2022-10-09 20:51:25 +0100

    Exercise 2 & 3 attempted

--- things/models.py
+++ things/models.py
@@ -5,5 +5,5 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, unique = True)
     description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
-        validators=[MinValueValidator(0),MaxValueValidator(50)]
+        validators=[MinValueValidator(0),MaxValueValidator(100)]
     )
